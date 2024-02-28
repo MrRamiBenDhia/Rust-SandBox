152ae1f42c0451c89118ad809c79b51ee5854abb
Rami Ben Dhia <rami.ben-dhia@ads.lan>
2024-02-28 17:33:27 +0100

    adding the # id for done items

--- my-todo-list/src/main.rs
+++ my-todo-list/src/main.rs
@@ -6,23 +6,21 @@ use std::fs::OpenOptions;
 use std::io::Write;
 use std::io::{self, BufRead};
 
-
-
 #[derive(Debug)]
-enum Operation{
-    Add, Done
+enum Operation {
+    Add,
+    Done,
 }
 
 fn main() {
     printcoln!("[s|cyan]Hello world [magenta]!");
-    
+
     let mut operation = Operation::Add;
 
-    let list_todo = read_from_cmd();
+    let list_todo = read_from_cmd(&mut operation);
+    println!("Operation after = {:?}", operation);
 
-    println!("Operation= {:?}",operation);
-
-    add_to_file(&list_todo);
+    add_to_file(&list_todo, &operation);
     let loaded_list_todo = read_from_file();
 
     // println!("Loaded list : {:?}",loaded_list_todo);
@@ -30,11 +28,6 @@ fn main() {
     println!("Length list : {}", loaded_list_todo.len());
 }
 
-// fn write_to_file() {
-//     let mut file = File::create("Data.txt").expect("Opps");
-//     file.write_all("Hello there \n".as_bytes())
-//         .expect("Fama mochkla");
-// }
 fn read_from_file() -> Vec<String> {
     let file = File::open("data.txt").expect("Failed to open file");
     let reader = io::BufReader::new(file);
@@ -47,15 +40,24 @@ fn read_from_file() -> Vec<String> {
 
     lines
 }
-fn add_to_file(list_todo: &Vec<String>) {
+
+fn add_to_file(list_todo: &Vec<String>, operation: &Operation) {
     let mut file = OpenOptions::new()
         .append(true)
         .open("data.txt")
         .expect("cannot open file");
 
     for item in list_todo {
-        file.write_all((item.to_owned() + "\n").as_bytes())
-            .expect("write failed");
+        match operation {
+            Operation::Done => {
+                file.write_all(("#".to_owned() + item + "\n").as_bytes())
+                    .expect("write failed");
+            }
+            _ => {
+                file.write_all((item.to_owned() + "\n").as_bytes())
+                    .expect("write failed");
+            }
+        }
     }
 
     println!("file append success");
@@ -65,22 +67,33 @@ fn print_list(list_todo: &Vec<String>) {
     println!("Here is the complete list : {:?}", list_todo);
 }
 
-fn read_from_cmd() -> Vec<String> {
+fn read_from_cmd(mut operation: &mut Operation) -> Vec<String> {
+    println!("Operation before = {:?}", operation);
 
-  
     let cmd_line = std::env::args();
-    println!("Num of elements to add in todo is :{}", cmd_line.len() - 1);
+    println!("Num of elements to add in todo is : {}", cmd_line.len() - 1);
 
     let mut list_todo = Vec::<String>::new();
     let mut has_read_first_arg = false;
+    let mut has_read_second_arg = false;
 
     for arg in cmd_line {
         if has_read_first_arg {
-            //skip the first argument since it is the exe file name
-            list_todo.push(arg)
+            // skip the first argument since it is the exe file name
+            if has_read_second_arg {
+                list_todo.push(arg);
+            } else {
+                println!("here is the second thing: {}", arg);
+                match arg.as_str() {
+                    "add" => *operation = Operation::Add,
+                    "done" => *operation = Operation::Done,
+                    _ => println!("wake up"),
+                }
+            }
+            has_read_second_arg = true;
         }
         has_read_first_arg = true;
     }
     println!("New Items {:?}", list_todo);
-    return list_todo;
+    list_todo
 }
